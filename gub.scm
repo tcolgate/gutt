1700e4545c5ef78a7ec1587613959144e1ac80e8
Tristan Colgate <tcolgate@gmail.com>
2013-07-28 15:56:47 +0100

    Start of a working xmlrpc over ssl client

--- gub.scm
+++ gub.scm
@@ -6,42 +6,68 @@
 (use-modules (ncurses curses)
              (srfi srfi-1)
              (srfi srfi-9)
+             (ice-9 receive)
              (srfi srfi-39)
              (web uri)
              (web client)
+             (web request)
+             (web response)
              (xmlrpc)
-             (gnutls))
+             (gnutls)
+             (sxml simple)
+             (rnrs bytevectors))
 
-(define uri (string->uri "https://google.com:443/"))
 
-(let ((client (make-session connection-end/client)))
+(define *priority* "NORMAL")
+(define *dh-bits* 1024)
 
- ;; Use the default settings.
+;(define uri (string->uri "https://www.google.com/"))
+(define uri (string->uri "https://bugzilla.mediagraft.com:1443/xmlrpc.cgi"))
+
+(let ((client (make-session connection-end/client))
+      (sock   (open-socket-for-uri uri)) 
+      (cred   (make-certificate-credentials)))
+
+ (set-log-level! 0)
  (set-session-default-priority! client) 
+ (set-certificate-credentials-x509-trust-file! cred "/etc/ssl/certs/ca-certificates.crt" x509-certificate-format/pem)
+ (set-session-credentials! client cred) 
 
- ;; Don't use certificate-based authentication.
- (set-session-certificate-type-priority! client '()) 
-
- ;; Request the "anonymous Diffie-Hellman" key exchange method.
- (set-session-kx-priority! client (list kx/anon-dh)) 
- 
- ;; Specify the underlying socket.
- (set-session-transport-fd! client (fileno (open-socket-for-uri uri))) 
- 
- ;; Create anonymous credentials.
- (set-session-credentials! client
-                           (make-anonymous-client-credentials)) 
- ;; Perform the TLS handshake with the server.
+ (set-session-transport-fd! client (fileno sock)) 
  (handshake client) 
 
- ;; Send data over the TLS record layer.
- (display  (http-get 
-   uri
-   #:port (session-record-port client)
-   #:keep-alive? #t )) 
+ (let* ((body  
+          (string->utf8
+            (call-with-output-string
+              (lambda (p) (sxml->xml (sxmlrpc (request 'identify "hello")) p)))))
+        (body-len (bytevector-length body))
+        (port (session-record-port client))
+        (req (build-request uri
+                            #:method 'POST
+                            #:port  port
+                            #:headers `((content-type . (text/xml))
+                                        (content-length . ,body-len))))) 
 
- ;; Terminate the TLS session.
- (bye client close-request/rdwr)) 
+   (write-request-body
+     (write-request req port)
+     body) 
+   ;; Flush port
+   (force-output port) 
+   ;; Read and return server response
+   (display (utf8->string (read-response-body (read-response port))))
+   (newline) 
+
+   ;; Send data over the TLS record layer.
+   ;   (receive (res body) 
+   ;                (http-get uri
+   ;                          #:port 
+   ;                          #:keep-alive? #t )
+   ;                (bye client close-request/rdwr)
+   ;                (display res) (newline))
+
+   ;; Terminate the TLS session.
+
+   (close sock))) 
 
 
  
